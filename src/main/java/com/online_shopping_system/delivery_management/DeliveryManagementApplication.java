7191d3b6d1bc99778a9c4cd8eaeb29a70ee7adeb
Rahman2001 <rahmanrejepov777@gmail.com>
2024-02-29 13:42:43 +0300

    updated pom

--- src/main/java/com/online_shopping_system/delivery_management/DeliveryManagementApplication.java
+++ src/main/java/com/online_shopping_system/delivery_management/DeliveryManagementApplication.java
@@ -1,2 +1,13 @@
 package com.online_shopping_system.delivery_management;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryManagementApplication.class, args);
+    }
+
+}
